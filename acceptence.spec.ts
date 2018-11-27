7c3a836b6343c9509bd04186aca45c72771e718d
Oliver Bell <freshollie@gmail.com>
2018-11-27 11:48:09 +0000

    Increase wait time for server to be up

--- acceptence.spec.ts
+++ acceptence.spec.ts
@@ -3,13 +3,13 @@ import { execSync } from 'child_process';
 
 const HOST = 'http://localhost:2737';
 
-jest.setTimeout(10000);
+jest.setTimeout(20000);
 
 beforeAll((done) => {
   execSync('docker-compose -f docker-compose.acceptence.yml up -d');
 
   // Wait for server to be up
-  setTimeout(done, 5000);
+  setTimeout(done, 10000);
 });
 
 afterAll(() => {
